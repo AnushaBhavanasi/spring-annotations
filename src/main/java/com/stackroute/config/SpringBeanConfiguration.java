1e2847492ed6ddec966bfec54bdf92ac3e24b048
anusha <vanushabhavanasi@gmail.com>
2019-05-27 16:55:49 +0530

    "task4 using spring annotations"

--- src/main/java/com/stackroute/config/SpringBeanConfiguration.java
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -1,8 +1,12 @@
 package com.stackroute.config;
 
+import awareInterface.ApplicationContextAwareDemo;
+import awareInterface.BeanFactoryAwareDemo;
+import awareInterface.BeanNameAwareDemo;
 import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
@@ -18,4 +22,18 @@ public class SpringBeanConfiguration {
     public Movie getMovie(){
         return new Movie();
     }
+    @Bean(name="applicationContextAwareDemo")
+    public ApplicationContextAwareDemo getApplicationContextAwareDemo(){
+        return new  ApplicationContextAwareDemo();
+    }
+    @Bean(name="beanFactoryAwareDemo")
+    public BeanFactoryAwareDemo getBeanFactoryAwareDemo(){
+        return new BeanFactoryAwareDemo();
+    }
+    @Bean(name="beanNameAwareDemo")
+    public BeanNameAwareDemo getBeanNameAwareDemo(){
+        return new BeanNameAwareDemo();
+    }
+
+
 }
