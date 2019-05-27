74b07782393b3d9bcfaa197392de85b2ef43732a
anusha <vanushabhavanasi@gmail.com>
2019-05-27 14:33:29 +0530

    "task2 using spring annotations"

--- src/main/java/com/stackroute/MainClass.java
+++ src/main/java/com/stackroute/MainClass.java
@@ -4,17 +4,23 @@ import com.stackroute.config.MainConfig;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
 public class MainClass
 {
     public static void main( String[] args )
     {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register(MainConfig.class);
-        ctx.refresh();
-        Movie movie1 = (Movie) ctx.getBean("movie1");
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.register(MainConfig.class);
+        annotationConfigApplicationContext.refresh();
+        //checking the default scope
+        Movie movie = (Movie) annotationConfigApplicationContext.getBean("movie");
+        //System.out.println(movie);
+        Movie movie1 = (Movie) annotationConfigApplicationContext.getBean("movie");
         System.out.println(movie1);
-        Movie movie = (Movie) ctx.getBean("movie");
-        System.out.println(movie);
+        System.out.println(movie==movie1);
+        //checking the scope using prototype
+        Movie movie2 = (Movie) annotationConfigApplicationContext.getBean("movie1");
+        System.out.println(movie2);
+        System.out.println(movie2==movie);
+
     }
 }
--- src/main/java/com/stackroute/config/SpringBeanConfiguration.java
+++ src/main/java/com/stackroute/config/SpringBeanConfiguration.java
@@ -4,11 +4,12 @@ import com.stackroute.domain.Actor;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBeanConfiguration {
-    public Actor getActor(){
+   /* public Actor getActor(){
         Actor actor=new Actor();
         actor.setActorName("latika");
         actor.setActorGender("female");
@@ -19,12 +20,17 @@ public class SpringBeanConfiguration {
     @Bean(name="movie1")
     public Movie getActor2(){
         return new Movie(getActor());
-    }
+    }*/
    @Bean(name = "movie")
   public Movie getActor1(){
        Actor actor=new Actor("AlluArjun","male",33);
-
-
       return new Movie(actor);
+    }
+
+    @Bean(name = "movie1")
+    @Scope("prototype")
+    public Movie getActor2(){
+        Actor actor=new Actor("Arjun","male",26);
+        return new Movie(actor);
     }
 }
